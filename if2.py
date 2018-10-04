23bbf04741466a6d89b3b29fa8054482784ea4b2
miko85 <michal.kohoutek@pm.me>
2018-10-04 10:37:57 +0200

    first commit

---
+++ if2.py
@@ -0,0 +1,19 @@
+obrazec = input('Počítám čtverec nebo kruh? ')
+square = obrazec == 'čtverec'
+circle = obrazec == 'kruh'
+if square == True:
+    strana = float(input('Zadej stranu čtverce: '))
+    print()
+    print("Obvod čtverce se stranou", strana, "cm je", 4 * strana, "cm.")
+    print("Obsah čtverce se stranou", strana, "cm je", strana ** 2, "cm2.")
+elif circle == True:
+    polomer = float(input('Zadej poloměr kruhu: '))
+    from math import pi
+    print()
+    print("Obvod kruhu s poloměrem", polomer, "cm je", 2 * pi * polomer, "cm.")
+    print("Obsah kruhu s poloměrem", polomer, "cm je", pi * polomer ** 2, "cm2.")
+else:
+    print('Vyber čtverec nebo kruh.')
+print()
+print()
+print('Nashledanou...')
